2a985bb618287b8d6f4ca9c0c76248d3b54dc17a
Hussnain Younas <FindHussnain@gmail.com>
2022-05-13 14:50:19 +0500

    Fix Form Input IDs

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,11 +31,13 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map( (meal) => (
   <MealItem 
+    id={meal.id}
     key={meal.id} 
     name={meal.name} 
     price={meal.price} 
     description={meal.description} 
   />));
+  
   return <section className={classes.meals}>
     <Card>
       <ul> {mealsList} </ul>
